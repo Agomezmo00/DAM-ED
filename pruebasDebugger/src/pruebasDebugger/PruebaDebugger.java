22514a54462415ede6c58134d9956d11104ae11a
agomezmo00 <agomezmo00@gmail.com>
2019-01-18 18:00:54 +0100

    testing classes

---
+++ pruebasDebugger/src/pruebasDebugger/PruebaDebugger.java
@@ -0,0 +1,11 @@
+package pruebasDebugger;
+
+public class PruebaDebugger {
+
+    public static void main(String[] args) {
+        ClaseParaDepurar clase = new ClaseParaDepurar();
+        double valor = clase.echaCuentas();
+        System.out.println("Sale "+valor);
+    }
+
+}
